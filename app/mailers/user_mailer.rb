dfa9dc45ce290a96efb9531ae76f3b4f988c3a71
lcbunch <lisabunch6@gmail.com>
2014-07-08 12:11:33 -0600

    trying to get mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "http://floating-oasis-7973.herokuapp.com"
+  default from: "http://floating-oasis-7973.herokuapp.com/"
 
   def welcome(user_email)
     @user_email = user_email
